6cdc486d9cc92a6c3debfef50af6e9bf63c5c5ae
Dzikran <sjdn.dzikran@gmail.com>
2025-08-04 08:10:49 +0700

    fix(grade): now grade starts from '-' to 'A'

--- src/components/ui/Card/CardHasil.tsx
+++ src/components/ui/Card/CardHasil.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 function CardHasil({ inspectionSummary, overallRating, urlPdf }: any) {
   function getGradeLabel(score: number): string {
     const gradingScale: { [key: number]: string } = {
-      0: "E",
-      1: "D-",
-      2: "D",
-      3: "C-",
-      4: "C",
-      5: "B-",
-      6: "B",
-      7: "B+",
-      8: "A-",
-      9: "A",
+      0: "-",
+      1: "E",
+      2: "D-",
+      3: "D",
+      4: "C-",
+      5: "C",
+      6: "B-",
+      7: "B",
+      8: "B+",
+      9: "A-",
       10: "A",
     };
 
